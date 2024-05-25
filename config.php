7fdbb376037874865b4f278b9f25490db3e2e2d7
Walisson Souza <86925855+Walissongs@users.noreply.github.com>
2024-05-25 19:45:32 -0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Armazena credenciais SMTP seguramente
-define('SMTP_HOST', 'smtp.mailersend.net');
-define('SMTP_USERNAME', 'MS_Sm3XVP@trial-3zxk54vywnp4jy6v.mlsender.net');
-define('SMTP_PASSWORD', '4sjeAb2KIbz0uiB5');
-define('MAIL_FROM', 'your-email@example.com');
-define('MAIL_FROM_NAME', 'Your Name');
-define('MAIL_TO', 'walisongoncalves23@gmail.com');
